d1c729ea40aa684ab6b6942c56c45d380448e544
Timothy J Fontaine <tjfontaine@gmail.com>
2008-12-10 13:18:24 -0500

    bring benchmark up to speed, but it should go back to file based

--- benchmark.py
+++ benchmark.py
@@ -22,33 +22,60 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE
 
-import sys, os
-from zpyzpr import ZpyZpr, GzipWorker, Bzip2Worker
+import sys, os, hashlib, StringIO, zlib, struct
+from zpyzpr.gzip import Gzip, GZIP_HEADER
+from zpyzpr.bzip2 import Bzip2
 from datetime import datetime
 
+def decompress(result):
+  parts = result.split(GZIP_HEADER)
+  data = ''
+  idx = 1
+  while idx < len(parts):
+    part = parts[idx]
+    start = 0
+    end   = len(part)-struct.calcsize('<II')
+    data += zlib.decompress(part[start:end], -zlib.MAX_WBITS)
+    idx += 1
+  return data
+
 if __name__ == '__main__':
-  block_sizes = [1024, 10*1024, 100*1024, 1000*1024, 10*1024*1024]
-  threads = [1, 2, 3, 4, 8, 16]
+  block_sizes = [10*1024, 100*1024, 1000*1024, 10*1024*1024]
+  threads = [2, 3, 4, 8, 16]
   compression = 6
 
   global_start = datetime.now()
-  original_size = os.stat(sys.argv[1]).st_size
+
+  source_string = open('/dev/urandom', 'rb').read(100*1024*1024)
+  source_hash = hashlib.sha1(source_string)
+  original_size = len(source_string)
 
   for thread in threads:
     for block_size in block_sizes:
       begin = datetime.now()
-      zz = ZpyZpr(worker=GzipWorker, threads=thread, block_size=block_size)
-      a = open(sys.argv[1], 'rb')
-      b = open(sys.argv[2], 'wb')
-      zz.start(a, b)
+      zz = Gzip(threads=thread, block_size=block_size)
+      a = StringIO.StringIO(source_string)
+      b = StringIO.StringIO()
+      zz.compressStream(a, b)
       zz.flush()
+      end = datetime.now()
+      data = b.getvalue()
+      size = len(data)
+      data = decompress(data)
+      result = hashlib.sha1(data)
+      data = None
+      del data
+
+      if source_hash.hexdigest() != result.hexdigest():
+        sys.stderr.write('src:%s%s' % (source_hash.hexdigest(), os.linesep))
+        sys.stderr.write('dst:%s%s' % (result.hexdigest(), os.linesep))
+        sys.stderr.write('Not the same hash as original' + os.linesep)
+        sys.exit(2)
+
       a.close()
       b.close()
-      end = datetime.now()
-      size = os.stat(sys.argv[2]).st_size
       sys.stderr.write('Time: %s | Size: %d\t| Threads: %d\t| Block Size: %d' % (str(end-begin), size-original_size,
                         thread, block_size)+os.linesep)
-      os.remove(sys.argv[2])
 
   end = datetime.now()
   sys.stderr.write('Total Time: %s' % str(end - global_start)+os.linesep)
